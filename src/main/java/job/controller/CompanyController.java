e924558e828f0ce2f89c35679de591c52e6f2303
wlsdyd0922 <wlsdyd0922@gmail.com>
2018-03-12 15:04:07 +0900

    213321321

--- src/main/java/job/controller/CompanyController.java
+++ src/main/java/job/controller/CompanyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CompanyController {
-	@RequestMapping("/기업")
+	@RequestMapping("/기업임")
 	public String company() {
 		return null;
 	}
