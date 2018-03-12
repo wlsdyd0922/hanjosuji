d2ad69f24c47faa410004f899acb06f1ab6fffb2
wlsdyd0922 <wlsdyd0922@gmail.com>
2018-03-12 14:26:08 +0900

    123

---
+++ src/main/java/job/controller/CompanyController.java
@@ -0,0 +1,12 @@
+package job.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CompanyController {
+	@RequestMapping("/기업")
+	public String company() {
+		return null;
+	}
+}
