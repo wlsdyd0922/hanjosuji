04cedf9ce1b6dd2c02ab6f67121edd4f6abe3f31
wlsdyd0922 <wlsdyd0922@gmail.com>
2018-03-13 12:21:57 +0900

    123

--- src/main/java/job/controller/LogInController.java
+++ src/main/java/job/controller/LogInController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
 public class LogInController {
-	@RequestMapping("/member/login")
+	@RequestMapping("member/login")
 	public String LogIn() {
 		return "login";
 	}
