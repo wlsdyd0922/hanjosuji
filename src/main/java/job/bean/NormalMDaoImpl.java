31cd1966b2bd84b8ab14a6ae4a03a2d00f478228
KTH <rlzxogus@naver.com>
2018-03-13 14:44:14 +0900

    member dao,dto

---
+++ src/main/java/job/bean/NormalMDaoImpl.java
@@ -0,0 +1,79 @@
+package job.bean;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+public class NormalMDaoImpl implements NormalMDao{
+	private JdbcTemplate jdbcTemplate;
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	private RowMapper<NormalMDto1> mapper = (rs, index)->{
+		return new NormalMDto1(rs);
+	};
+	private ResultSetExtractor<NormalMDto1> extractor = (rs)->{
+		if(rs.next()) return new NormalMDto1(rs);
+		else return null;
+	};
+	@Override
+	//EMAIL	PASSWORD GENDER	BIRTH	CAREER	EDU	PRIZE	CERTIFICATE	PORTFOLIO	REG	PHONE	PWQUIZ	PWANS
+	public void insert(NormalMDto1 nmdto) {
+		String sql = "insert into NormalM values(?,?,?,?,?,?,?,?,?,sysdate,?,?,?)";
+		Object[] args= new Object[] {
+				nmdto.getEmail(),nmdto.getPassword(),nmdto.getGender(),nmdto.getBirth(),
+				nmdto.getCareer(),nmdto.getEdu(),nmdto.getPrize(),
+				nmdto.getCertificate(),nmdto.getPortfolio(),nmdto.getReg(),nmdto.getPhone(),
+				nmdto.getPwQuiz(),nmdto.getPwAns()};
+			jdbcTemplate.update(sql,args);
+	}
+	@Override
+	public boolean edit(NormalMDto1 nmdto) {
+		String sql = "update NormalM set password=?, birth=?, career=?, "
+				+ "edu=?, prize=?, certificate=?,portfolio=?,phone=?,pwQuiz=?,"
+				+ "pwAns=? where email=?";
+		Object[] args = new Object[] {
+			nmdto.getPassword(),nmdto.getBirth(),nmdto.getCareer(),nmdto.getEdu(),nmdto.getPrize(),
+			nmdto.getCertificate(),nmdto.getPortfolio(),nmdto.getPhone(),nmdto.getPwQuiz(),nmdto.getPwAns(),
+			nmdto.getEmail()
+		};
+		return jdbcTemplate.update(sql, args) > 0;
+	}
+	@Override
+	public boolean drop(NormalMDto1 nmdto) {
+		String sql = "delete NormalM where email=? and password=?";
+		return jdbcTemplate.update(sql,nmdto.getEmail(),nmdto.getPassword())>0;
+	}
+	@Override
+	public String getEmail(NormalMDto1 nmdto) {
+		String sql = "select email from NormalM where name=? and phone=?";
+		Object[] args = new Object[] {nmdto.getName(),nmdto.getPhone()};
+		return jdbcTemplate.queryForObject(sql,args,String.class);	
+	}
+	@Override
+	public String getPw(NormalMDto1 nmdto) {
+		String sql = "select password from NormalM where email=? and phone=? and pwquiz=? and pwans=?";
+		Object[] args = new Object[] {
+			nmdto.getEmail(),nmdto.getPhone(),
+			nmdto.getPwQuiz(),nmdto.getPwAns()	
+		};
+		return jdbcTemplate.queryForObject(sql, args,String.class);
+	}
+	@Override
+	public NormalMDto1 info(String email) {
+		String sql = "select * from NormalM where=?";
+		return jdbcTemplate.query(sql,extractor,email);
+	}
+	@Override
+	public List<NormalMDto1> list() {
+		String sql = "select * from NormalM order by email";
+		return jdbcTemplate.query(sql, mapper);
+	}
+	@Override
+	public boolean login(String email, String password) {
+		String sql = "select * from NormalM where email=? and password=?";
+		return jdbcTemplate.query(sql, extractor, email, password) != null;
+	}
+}
---
+++ src/main/java/job/bean/NormalMDto1.java
@@ -0,0 +1,132 @@
+package job.bean;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NormalMDto1 {
+	private String email;		
+	private String password;
+	private String gender;
+	private String birth;
+	private String career;			//경력
+	private String edu;				//학력
+	private String prize;			//수상경력
+	private String certificate;		//자격증
+	private String portfolio;		//포트폴리오 파일명
+	private String reg;
+	private String phone;
+	private String pwQuiz;
+	private String pwAns;
+	private String name;
+	
+	public NormalMDto1() {
+		super();
+	}
+	
+	public NormalMDto1(ResultSet rs) throws SQLException
+	{
+		setEmail(rs.getString("email"));
+		setPassword(rs.getString("password"));
+		setGender(rs.getString("gender"));
+		setBirth(rs.getString("birth"));
+		setCareer(rs.getString("career"));
+		setEdu(rs.getString("edu"));
+		setPrize(rs.getString("prize"));
+		setCertificate(rs.getString("certificate"));
+		setPortfolio(rs.getString("portfolio"));
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getCareer() {
+		return career;
+	}
+	public void setCareer(String career) {
+		this.career = career;
+	}
+	public String getEdu() {
+		return edu;
+	}
+	public void setEdu(String edu) {
+		this.edu = edu;
+	}
+	public String getPrize() {
+		return prize;
+	}
+	public void setPrize(String prize) {
+		this.prize = prize;
+	}
+	public String getCertificate() {
+		return certificate;
+	}
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+	public String getPortfolio() {
+		return portfolio;
+	}
+	public void setPortfolio(String portfolio) {
+		this.portfolio = portfolio;
+	}
+
+	public String getReg() {
+		return reg;
+	}
+
+	public void setReg(String reg) {
+		this.reg = reg;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPwQuiz() {
+		return pwQuiz;
+	}
+
+	public void setPwQuiz(String pwQuiz) {
+		this.pwQuiz = pwQuiz;
+	}
+
+	public String getPwAns() {
+		return pwAns;
+	}
+
+	public void setPwAns(String pwAns) {
+		this.pwAns = pwAns;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
