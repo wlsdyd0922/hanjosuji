93153561180ce7a7e0bb8200f50b42c230ab153a
suzy <tnwl9110@gmail.com>
2018-03-13 11:32:21 +0900

    123

---
+++ src/main/java/work/company/bean/CompanyDto.java
@@ -0,0 +1,74 @@
+package work.company.bean;
+
+public class CompanyDto {
+	private String email; 
+	private String password; 
+	private String company; 
+	private String ceo; 
+	private String birth; 
+	private String sales; 
+	private String type;
+	private String location;
+	private String industry; 
+	private int employee;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getCeo() {
+		return ceo;
+	}
+	public void setCeo(String ceo) {
+		this.ceo = ceo;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getSales() {
+		return sales;
+	}
+	public void setSales(String sales) {
+		this.sales = sales;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getIndustry() {
+		return industry;
+	}
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+	public int getEmployee() {
+		return employee;
+	}
+	public void setEmployee(int employee) {
+		this.employee = employee;
+	} 
+}
