402c236ddac682c9befab0030a8bacefd3d1095d
owner <owner@14.37.184.18>
2018-03-13 22:36:50 +0900

    login 결과에 따른 페이지 전환 리다이렉트 구현

--- src/main/java/job/controller/member/LogInController.java
+++ src/main/java/job/controller/member/LogInController.java
@@ -17,7 +17,7 @@ import job.bean.NormalMDto;
 @Controller()
 public class LogInController {
 	private NormalMDaoImpl NMdao = new NormalMDaoImpl();
-	
+
 	public NormalMDaoImpl getNMdao() {
 		return NMdao;
 	}
@@ -33,37 +33,49 @@ public class LogInController {
 		return "member/login";
 	}
 
+	@RequestMapping("member/login_ok")
+	public String LogIn_OK() {
+		return "member/login_ok";
+	}
+
+	@RequestMapping("member/login_nok")
+	public String LogIn_NOK() {
+		return "member/login_nok";
+	}
+
 	@RequestMapping(value = "member/login", method = RequestMethod.POST)
 	public String LogIn(NormalMDto NMdto, HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
+
+		// DB연결
+
+		
+		
+		// 디버깅용-----------------------------------
 		String id = NMdto.getEmail();
 		String pw = NMdto.getPassword();
 		boolean chkid = NMdto.isRememberId();
-		// DB연결
-		
-		////////////////////////
+
 		log.debug("id = {}", id);
 		log.debug("pw = {}", pw);
 		log.debug("chk = {}", chkid);
+		// ----------------------------------------
 		Cookie ck = new Cookie("rememberId", id);
 		if (chkid) {
 			// 쿠키 생성
-			ck.setMaxAge(4*7*24*60*60);
+			ck.setMaxAge(4 * 7 * 24 * 60 * 60);
 		} else {
 			ck.setMaxAge(0);
 		}
 
 		response.addCookie(ck);
-		
-//		boolean result = NMdao.login(id, pw);
+
+		// boolean result = NMdao.login(id, pw);
 		boolean result = true;
-		if(result == true)
-		{
-			session.setAttribute("accept",id);//accept라는 이름으로 세션에 id를 저장한다.
+		if (result == true) {
+			session.setAttribute("accept", id);// accept라는 이름으로 세션에 id를 저장한다.
 			return "redirect:login_ok";
-		}
-		else
-		{
+		} else {
 			return "redirect:login_nok";
 		}
 	}
