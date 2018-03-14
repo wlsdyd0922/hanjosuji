44d6523865792abce8d65d0cc2fc623d3f6182b3
wlsdyd0922 <wlsdyd0922@gmail.com>
2018-03-14 10:53:40 +0900

    123

--- src/main/java/job/controller/member/Register_detailController.java
+++
@@ -1,9 +0,0 @@
-package job.controller.member;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class Register_detailController {
-
-}
