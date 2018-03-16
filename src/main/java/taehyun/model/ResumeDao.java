d98e4d1bcb30ac318c6505f5e901cd187825ebf3
KTH <rlzxogus@naver.com>
2018-03-16 09:22:03 +0900

    이력서dao 수정

--- src/main/java/taehyun/model/ResumeDao.java
+++ src/main/java/taehyun/model/ResumeDao.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import taehyun.bean.ResumeDto;
 
-@Repository
+@Repository 
 public interface ResumeDao {
 	void insert(ResumeDto idto);
 	List<ResumeDto> getList();
--- src/main/java/taehyun/model/ResumeDaoImpl.java
+++ src/main/java/taehyun/model/ResumeDaoImpl.java
@@ -1,7 +1,7 @@
 package taehyun.model;
 
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
