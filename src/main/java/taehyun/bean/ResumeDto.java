8392ced4e3b961425ef41f9d19e75a121288ce57
KTH <rlzxogus@naver.com>
2018-03-19 13:23:00 +0900

    resume dao,dto

--- src/main/java/taehyun/bean/ResumeDto.java
+++ src/main/java/taehyun/bean/ResumeDto.java
@@ -4,58 +4,109 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ResumeDto {
-	private String savename;
-	private String filename;
-	private String filetype;
-	private long filelen;
-	private String reg;
-	private String author;
-	
-	public String getSavename() {
-		return savename;
+	private String title;
+	private String career;
+	private String edu;
+	private String salary;
+	private String pr;
+	private String portfolio;
+	private String certi;
+	private String prize;
+	private String email;
+	private int boardno;
+	private String face;
+	public ResumeDto(ResultSet rs) throws SQLException {
+		setTitle(rs.getString("title"));
+		setCareer(rs.getString("career"));
+		setEdu(rs.getString("edu"));
+		setSalary(rs.getString("salary"));
+		setPr(rs.getString("pr"));
+		setPortfolio(rs.getString("portfolio"));
+		setCerti(rs.getString("certi"));
+		setPrize(rs.getString("prize"));
+		setEmail(rs.getString("email"));
+		setBoardno(rs.getInt("boardno"));
+		setFace(rs.getString("face"));
 	}
-	public void setSavename(String savename) {
-		this.savename = savename;
-	}
-	public String getFilename() {
-		return filename;
-	}
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
-	public String getFiletype() {
-		return filetype;
-	}
-	public void setFiletype(String filetype) {
-		this.filetype = filetype;
-	}
-	public long getFilelen() {
-		return filelen;
-	}
-	public void setFilelen(long filelen) {
-		this.filelen = filelen;
-	}
-	public String getReg() {
-		return reg;
-	}
-	public void setReg(String reg) {
-		this.reg = reg;
-	}
-	public ResumeDto() {
+	public ResumeDto(String title, String career, String edu, String salary, String pr, String portfolio, String certi,
+			String prize, String email, int boardno, String face) {
 		super();
+		this.title = title;
+		this.career = career;
+		this.edu = edu;
+		this.salary = salary;
+		this.pr = pr;
+		this.portfolio = portfolio;
+		this.certi = certi;
+		this.prize = prize;
+		this.email = email;
+		this.boardno = boardno;
+		this.face = face;
 	}
-	public String getAuthor() {
-		return author;
+	public String getTitle() {
+		return title;
 	}
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setTitle(String title) {
+		this.title = title;
 	}
-	public ResumeDto(ResultSet rs) throws SQLException{
-		setSavename(rs.getString("savename"));
-		setFilename(rs.getString("filename"));
-		setFiletype(rs.getString("filetype"));
-		setFilelen(rs.getLong("filelen"));
-		setReg(rs.getString("reg"));
-		setAuthor(rs.getString("author"));
+	public String getCareer() {
+		return career;
+	}
+	public void setCareer(String career) {
+		this.career = career;
+	}
+	public String getEdu() {
+		return edu;
+	}
+	public void setEdu(String edu) {
+		this.edu = edu;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getPr() {
+		return pr;
+	}
+	public void setPr(String pr) {
+		this.pr = pr;
+	}
+	public String getPortfolio() {
+		return portfolio;
+	}
+	public void setPortfolio(String portfolio) {
+		this.portfolio = portfolio;
+	}
+	public String getCerti() {
+		return certi;
+	}
+	public void setCerti(String certi) {
+		this.certi = certi;
+	}
+	public String getPrize() {
+		return prize;
+	}
+	public void setPrize(String prize) {
+		this.prize = prize;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBoardno() {
+		return boardno;
+	}
+	public void setBoardno(int boardno) {
+		this.boardno = boardno;
+	}
+	public String getFace() {
+		return face;
+	}
+	public void setFace(String face) {
+		this.face = face;
 	}
 }
