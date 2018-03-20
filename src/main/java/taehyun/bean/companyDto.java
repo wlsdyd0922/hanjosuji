c44d379f9739347c0ba22ab9802cdc1a9a33cdea
KTH <rlzxogus@naver.com>
2018-03-20 14:27:26 +0900

    companyDto 사라져서 다시 만듬

---
+++ src/main/java/taehyun/bean/companyDto.java
@@ -0,0 +1,104 @@
+package taehyun.bean;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class companyDto {
+	private String name;
+	private String industry;
+	private String ceo;
+	private String birth;
+	private String website;
+	private int employee;
+	private String type;
+	private int sales;
+	private String location;
+	private String imgname;
+	private String imgecnoding;
+	
+	public companyDto() {
+		super();
+	}
+	
+	public companyDto(ResultSet rs) throws SQLException {
+		setName(rs.getString("name"));
+		setIndustry(rs.getString("industry"));
+		setCeo(rs.getString("ceo"));
+		setBirth(rs.getString("birth"));
+		setWebsite(rs.getString("website"));
+		setEmployee(rs.getInt("employee"));
+		setType(rs.getString("type"));
+		setSales(rs.getInt("sales"));
+		setLocation(rs.getString("location"));
+		setImgname(rs.getString("imgname"));
+		setImgecnoding(rs.getString("imgecnoding"));
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIndustry() {
+		return industry;
+	}
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+	public String getCeo() {
+		return ceo;
+	}
+	public void setCeo(String ceo) {
+		this.ceo = ceo;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public int getEmployee() {
+		return employee;
+	}
+	public void setEmployee(int employee) {
+		this.employee = employee;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getSales() {
+		return sales;
+	}
+	public void setSales(int sales) {
+		this.sales = sales;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getImgname() {
+		return imgname;
+	}
+	public void setImgname(String imgname) {
+		this.imgname = imgname;
+	}
+	public String getImgecnoding() {
+		return imgecnoding;
+	}
+	public void setImgecnoding(String imgecnoding) {
+		this.imgecnoding = imgecnoding;
+	}
+	
+}
