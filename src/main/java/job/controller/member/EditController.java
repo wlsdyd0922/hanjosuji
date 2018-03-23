3702461f9536589d668e640db9210de881c97efe
204-21 <204-21@DESKTOP-K9U9BBQ>
2018-03-23 15:24:36 +0900

    11111

--- src/main/java/job/controller/member/EditController.java
+++ src/main/java/job/controller/member/EditController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import job.model.CompanyDaoImpl;
 import job.model.NormalMDaoImpl;
+import job.model.ResumeDaoImpl;
 
 @Controller
 @RequestMapping("member")
@@ -18,6 +19,8 @@ public class EditController {
 	private NormalMDaoImpl nmdao;
 	@Autowired
 	private CompanyDaoImpl cdao;
+	@Autowired
+	private ResumeDaoImpl rdao;
 	
 	@RequestMapping("edit_personal")
 	public String EditPersonal(HttpServletRequest request) {
@@ -31,19 +34,24 @@ public class EditController {
 	} 
 	@RequestMapping("edit_company")
 	public String EditCompany(HttpServletRequest request) {
-		String email = (String) request.getSession().getAttribute("accept");
-		request.setAttribute("nmdto", nmdao.info(email));
-		return "company/list";
+		String company = (String) request.getSession().getAttribute("company");
+		request.setAttribute("cdto", cdao.searchTarget(company));
+		return "company/edit_compnay";
 	} 
 	@RequestMapping(value="edit_company",method=RequestMethod.POST)
 	public String EditCompany() {
-		return "member/edit_compnay";
+		return "company/edit_compnay";
 	} 
 	
-	
 	@RequestMapping("edit_resume")
-	public String EditDetailResum() {
+	public String EditDetailResume(HttpServletRequest request) {
+		String author = (String) request.getSession().getAttribute("author");
+		request.setAttribute("rdto", rdao.searchTarget(author));
 		return "member/edit_resume";
+	} 
+	@RequestMapping(value="edit_resume",method=RequestMethod.POST)
+	public String EditDetailResume() {
+		return "company/edit_resume";
 	} 
 
 	@RequestMapping("edit_introduction_paper")
